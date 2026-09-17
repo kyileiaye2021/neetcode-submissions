a78ac34749eb289ae67c2e652130ed9d8e47ea7f
Kyi Lei Aye (She/ Her) <98507949+kyileiaye2021@users.noreply.github.com>
2026-09-17 01:45:36 -0700

    Add: string-encode-and-decode - submission-16

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-16.py
@@ -0,0 +1,56 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+
+        # ["h", 'L']
+        # hL
+        # 1#h1#L
+
+        # [" n", "#y"]
+        # '2 n2#y'
+
+        # ''
+        # ''
+
+        res_str = ''
+        for s in strs:
+            res_str += str(len(s)) + "#" + s
+
+        return res_str
+
+    def decode(self, s: str) -> List[str]:
+
+        # 1#h1#L
+        # ["h", 'L']
+
+        # ' n#y'
+        # [" n", "#y"]
+
+        # ''
+        # ['']
+
+        if s == '':
+            return []
+
+        res_lst = []
+        i = 0
+        j = 0
+
+        while i < len(s):
+
+            while s[j] != '#':
+                j += 1
+            print(i)
+            print(j)
+            count = int(s[i:j])
+            i = j + 1
+            j = i + count
+            res_lst.append(s[i:j])
+            i = j
+        return res_lst
+            
+
+
+
+
+
