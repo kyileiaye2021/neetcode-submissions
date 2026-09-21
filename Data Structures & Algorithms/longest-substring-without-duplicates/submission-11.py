a3954d2d87dcf88302b1b1d4e9282f4ce9b4f648
Kyi Lei Aye (She/ Her) <98507949+kyileiaye2021@users.noreply.github.com>
2026-09-21 10:41:38 -0700

    Add: longest-substring-without-duplicates - submission-11

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-11.py
@@ -0,0 +1,45 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+
+        # 2 poiner sliding window
+        # max window = 0
+        # set 
+        # i, j = 0, 0
+        # while j < len(s)
+        #   check if j ele in set
+        #       remove i ele from the set
+        #       i += 1
+        #   
+        #   add j ele to the set
+        #   j += 1
+        #   find the window size (j - i)
+        #   update max window
+        # return max window
+
+        # max_window = 0
+        # visited = set()
+        # i, j = 0, 0
+
+        # while j < len(s):
+        #     while s[j] in visited:
+        #         visited.remove(s[i])
+        #         i += 1
+
+        #     visited.add(s[j])
+        #     j += 1
+
+        #     curr_window = j - i
+        #     max_window = max(max_window, curr_window)
+
+        # return max_window
+
+        mp = {}
+        l = 0
+        res = 0
+
+        for r in range(len(s)):
+            if s[r] in mp:
+                l = max(l, mp[s[r]] + 1)
+            mp[s[r]] = r
+            res = max(res, r - l + 1)
+        return res
