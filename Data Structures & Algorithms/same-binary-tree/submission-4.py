8c69f0db4dae93b62afb046d3f5a627661a8c168
Kyi Lei Aye (She/ Her) <98507949+kyileiaye2021@users.noreply.github.com>
2026-09-25 03:51:43 -0700

    Add: same-binary-tree - submission-4

---
+++ Data Structures & Algorithms/same-binary-tree/submission-4.py
@@ -0,0 +1,57 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+
+        # happy cases
+        # p = 1
+        # q = 1
+        # true
+
+        # edge cases
+        # p = None, q = None
+        # true
+
+        # p = [4] q = None
+        # false
+
+        # p = [None], q = [7]
+        # false
+        
+        # base case
+        # when p and q None
+        #   return true
+
+        # if not p and q
+        #   return False
+
+        # if not q and p
+        #   return false
+
+        # if p.val != q.val
+        #    return false
+
+        # left = recur(p.left, q.left)
+        # right = recur(p.right, q.right)
+        # return left and right
+
+        def recur(p, q):
+            if not p and not q:
+                return True
+
+            if not p or not q:
+                return False
+
+            if p.val != q.val:
+                return False
+
+            left = recur(p.left, q.left)
+            right = recur(p.right, q.right)
+            return left and right
+
+        return recur(p, q)
